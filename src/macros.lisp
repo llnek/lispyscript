3b06e4964e7d0fcf5d16dfc3eceb57612cd30152
Kenneth Leung <ccllnekk@gmail.com>
2017-08-29 23:46:59 -0700

    kl

--- src/macros.lisp
+++ src/macros.lisp
@@ -203,18 +203,18 @@
 ;;;;;;;;;;;;;;;; Monads ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defmacro m-identity ()
-  {
+  (hash-map
     bind (fn (mv mf) (mf mv))
-    unit (fn (v) v) } )
+    unit (fn (v) v) ))
 
 (defmacro m-maybe ()
-  {
+  (hash-map
     bind (fn (mv mf) (if (nil? mv) nil (mf mv)))
     unit (fn (v) v)
-    zero nil})
+    zero nil))
 
 (defmacro m-array ()
-  {
+  (hash-map
     bind (fn (mv mf)
               (reduce
                 (map mv mf)
@@ -226,20 +226,20 @@
           (reduce
             (Array.prototype.slice.call arguments)
             (fn (accum val) (accum.concat val))
-            []))})
+            []))))
 
 (defmacro m-state ()
-  {
+  (hash-map
     bind (fn (mv f)
               (fn (s)
                 (var l (mv s)
                      v (get l 0)
                      ss (get l 1))
                 ((f v) ss)))
-    unit (fn (v) (fn (s) [v, s]))})
+    unit (fn (v) (fn (s) [v, s]))))
 
 (defmacro m-continuation ()
-  {
+  (hash-map
     bind (fn (mv mf)
               (fn (c)
                 (mv
@@ -247,7 +247,7 @@
                     ((mf v) c)))))
     unit (fn (v)
                 (fn (c)
-                  (c v)))})
+                  (c v)))))
 
 (defmacro m-bind (binder bindings expr)
   (~binder (#slice@2 bindings)
--- x.lisp
+++ x.lisp
@@ -1,12 +1,14 @@
-2
-(var *xyz-poo* 3)
+(comment
+(var x [ 1 2 3 ])
+(var x [])
+(var x [[1 2 (* 2 3) [4 5]]])
 
-(defn xxxx () 
-      (* 1 *xyz-poo*))
+(var x { a 1 b 2 c 3 })
+(var x {})
+(var x { z { a 1 b { c 2 }  d (fn () (* 3 4))}})
+(m-identity))
+;;(let (a [1 2 3] b [3 4 5]) (+ a b))
 
-(def- *VERSION* "1.0.0"
-      *includePaths* []
-      *noSemiColon?* false
-      *indentSize* 3
-      *indent* -*indentSize*)
+(do-monad m-array (a [1 2 3] b [3 4 5]) (+ a b))
+
 
